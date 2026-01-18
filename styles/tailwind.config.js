4d079af6e1bcdbd4656b27a50269401c3b663b08
tirachrach950-sys <tirachrach950@gmail.com>
2026-01-18 18:11:08 +0100

    Update tailwind.config.js

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,1 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
